eeb6645c9d49365b324cbe63c8b4a79fae41d4d0
jhaugh42@gmail.com <jhaugh42@gmail.com>
2016-07-14 07:15:25 -0400

    ISSUE-12 - refactor. add concurrent-all and map-concurrent-all.

--- index.js
+++ index.js
@@ -3,5 +3,8 @@
 module.exports = {
     series: require('./lib/series.js').series,
     mapEach: require('./lib/map-each.js').mapEach,
-    concurrent: require('./lib/concurrent.js').concurrent
+    concurrent: require('./lib/concurrent.js').concurrent,
+    concurrentAll: require('./lib/concurrent-all.js').concurrentAll,
+    mapConcurrent: require('./lib/map-concurrent.js').mapConcurrent,
+    mapConcurrentAll: require('./lib/map-concurrent-all.js').mapConcurrentAll
 };
